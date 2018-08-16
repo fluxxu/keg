e07adc351d3d9521a085f7fdb225cf18962b42a0
Jerome Leclanche <jerome@leclan.ch>
2018-08-16 10:29:18 +0300

    Implement Keg.get_cached_cdns() to fetch CDN list from keg.db

--- keg/__init__.py
+++ keg/__init__.py
@@ -1,6 +1,7 @@
-from typing import Any, Tuple
+from typing import Any, List, Tuple
 
-from .http import HttpBackend
+from . import psv
+from .http import CDNs, HttpBackend, StateCache
 
 
 class Keg(HttpBackend):
@@ -43,3 +44,26 @@ class Keg(HttpBackend):
 		self.cache_db.commit()
 
 		return psvfile, response
+
+	def get_cached_psv(self, path: str, key: str, cache_dir: str) -> psv.PSVFile:
+		data = StateCache(cache_dir).read(path, key)
+		return psv.loads(data)
+
+	def get_cached_cdns(self, remote: str, cache_dir: str) -> List[CDNs]:
+		cursor = self.cache_db.cursor()
+		cursor.execute("""
+			SELECT digest
+			FROM responses
+			WHERE
+				remote = ? AND
+				path = ?
+			ORDER BY timestamp DESC
+			LIMIT 1
+		""", (remote, "/cdns"))
+		results = cursor.fetchone()
+		if not results:
+			# Fall back to querying live
+			return self.get_cdns()
+
+		psvfile = self.get_cached_psv("/cdns", results[0], cache_dir)
+		return [CDNs(row) for row in psvfile]
