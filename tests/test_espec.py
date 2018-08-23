c17f7a875a839adb95087ddc423c7faaf55dc731
Jerome Leclanche <jerome@leclan.ch>
2018-08-23 18:59:30 +0300

    Parse a ton of known encodings in tests

--- tests/test_espec.py
+++ tests/test_espec.py
@@ -1,17 +1,13 @@
 from keg import espec
 
+from . import get_resource
+
 
 def test_espec_grammar():
-	specs = [
-		"n",
-		"z",
-		"b:{1768=z,66443=n}",
-		"b:{164=z,16K*565=z,1656=z,140164=z}",
-		"b:{256K*=e:{237DA26C65073F42,06FC152E,z}}",
-		"b:{22=n,54=z,192=n,24576=n,128=n,16384=n,*=z}",
-		"b:{22=n,31943=z,211232=n,27037696=n,138656=n,17747968=n,*=z}",
-		"b:{16K*=z:{6,mpq}}",
-	]
+	with get_resource("encodings.txt", "r") as f:
+		data = f.read()
+
+	specs = data.splitlines()
 
 	for spec in specs:
 		espec.GRAMMAR.parse(spec)
