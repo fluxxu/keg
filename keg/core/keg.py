5a8968b53cf40b841ef612dc16edde4c58d45367
Jerome Leclanche <jerome@leclan.ch>
2018-08-26 02:43:48 +0300

    Move lots of app logic to keg.core.keg.Keg class

---
+++ keg/core/keg.py
@@ -0,0 +1,45 @@
+import os
+
+from .. import CacheableHttpRemote
+from ..cdn import LocalCDN
+from .config import KegConfig
+from .db import KegDB
+
+
+class Keg:
+	def __init__(self, path: str) -> None:
+		self.path = os.path.abspath(path)
+		self.objects_path = os.path.join(self.path, "objects")
+		self.fragments_path = os.path.join(self.path, "fragments")
+		self.response_cache_dir = os.path.join(self.path, "responses")
+		self.config_path = os.path.join(self.path, "keg.conf")
+		self.db_path = os.path.join(self.path, "keg.db")
+
+		self.initialized = os.path.exists(self.path)
+
+		if self.initialized:
+			self.db = KegDB(self.db_path)
+		else:
+			self.db = KegDB(":memory:")
+
+		self.config = KegConfig(self.config_path)
+		self.local_cdn = LocalCDN(self.objects_path, self.fragments_path)
+
+	def initialize(self) -> bool:
+		if not os.path.exists(self.path):
+			reinitialized = True
+			os.makedirs(self.path)
+		else:
+			reinitialized = False
+
+		self.config.initialize()
+
+		self.db = KegDB(self.db_path)
+		self.db.create_tables()
+
+		return reinitialized
+
+	def get_remote(self, remote: str) -> CacheableHttpRemote:
+		return CacheableHttpRemote(
+			remote, cache_dir=self.response_cache_dir, cache_db=self.db
+		)
