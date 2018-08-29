4370cc099910f433a816442e1e7d56327980a414
Jerome Leclanche <jerome@leclan.ch>
2018-08-29 06:30:49 +0300

    Simplify the fetch API by passing local/remote CDN to the Fetcher

--- keg/core/fetcher.py
+++ keg/core/fetcher.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from typing import IO, Any, Generator, Optional, Set, Type
+from typing import Generator, Optional, Set, Type
 
 from .. import blte, cdn
 from ..archive import ArchiveGroup
@@ -18,31 +18,20 @@ class FetchDirective:
 	def get_full_path(key: str) -> str:
 		raise NotImplementedError()
 
-	def __init__(
-		self,
-		key: str,
-		local_cdn: cdn.LocalCDN,
-		remote_cdn: cdn.RemoteCDN,
-		decryption_key: ArmadilloKey=None
-	) -> None:
+	def __init__(self, key: str, fetcher: "Fetcher") -> None:
 		self.key = key
-		self.local_cdn = local_cdn
-		self.remote_cdn = remote_cdn
-		self.decryption_key = decryption_key
-
-	def get_object(self, item: IO, verify: bool=False) -> Optional[Any]:
-		return None
+		self.fetcher = fetcher
 
 	def fetch(self, verify: bool=False) -> None:
 		path = self.get_full_path(self.key)
 		if not self.exists():
-			item = self.remote_cdn.get_item(path)
-			if self.decryption_key:
-				item = BytesIO(self.decryption_key.decrypt_object(self.key, item.read()))
-			self.local_cdn.save_item(item, path)
+			item = self.fetcher.remote_cdn.get_item(path)
+			if self.fetcher.decryption_key:
+				item = BytesIO(self.fetcher.decryption_key.decrypt_object(self.key, item.read()))
+			self.fetcher.local_cdn.save_item(item, path)
 
 	def exists(self) -> bool:
-		return self.key_exists(self.key, self.local_cdn)
+		return self.key_exists(self.key, self.fetcher.local_cdn)
 
 
 class ProductConfigFetchDirective(FetchDirective):
@@ -53,8 +42,8 @@ class ProductConfigFetchDirective(FetchDirective):
 	def fetch(self, verify: bool=False) -> None:
 		path = cdn.get_config_item_path(self.key)
 		if not self.exists():
-			item = self.remote_cdn.get_config_item(path)
-			self.local_cdn.save_item(item, path)
+			item = self.fetcher.remote_cdn.get_config_item(path)
+			self.fetcher.local_cdn.save_item(item, path)
 
 
 class ConfigFetchDirective(FetchDirective):
@@ -90,35 +79,19 @@ class FetchQueue:
 	def exists(self, key: str, local_cdn: cdn.LocalCDN) -> bool:
 		return self.directive_class.key_exists(key, local_cdn)
 
-	def drain(
-		self,
-		local_cdn: cdn.LocalCDN,
-		remote_cdn: cdn.RemoteCDN,
-		decryption_key: ArmadilloKey=None
-	) -> Generator[FetchDirective, None, None]:
+	def drain(self, fetcher: "Fetcher") -> Generator[FetchDirective, None, None]:
 		for key in sorted(self._queue):
-			if not self.exists(key, local_cdn):
-				yield self.directive_class(
-					key, local_cdn, remote_cdn, decryption_key
-				)
+			if not self.exists(key, fetcher.local_cdn):
+				yield self.directive_class(key, fetcher)
 				self.drained += 1
 			self._queue.remove(key)
 
 
 class Drain:
-	def __init__(
-		self,
-		name: str,
-		queue: FetchQueue,
-		local_cdn: cdn.LocalCDN,
-		remote_cdn: cdn.RemoteCDN,
-		decryption_key: ArmadilloKey=None
-	) -> None:
+	def __init__(self, name: str, queue: FetchQueue, fetcher: "Fetcher") -> None:
 		self.name = name
 		self.queue = queue
-		self.local_cdn = local_cdn
-		self.remote_cdn = remote_cdn
-		self.decryption_key = decryption_key
+		self.fetcher = fetcher
 
 	def __repr__(self):
 		return f"<{self.__class__.__name__}: {self.name} ({len(self)} items)>"
@@ -127,16 +100,20 @@ class Drain:
 		return len(self.queue._queue)
 
 	def drain(self) -> Generator[FetchDirective, None, None]:
-		yield from self.queue.drain(self.local_cdn, self.remote_cdn, self.decryption_key)
+		yield from self.queue.drain(self.fetcher)
 
 
 class Fetcher:
 	def __init__(
 		self,
 		version: Versions,
+		local_cdn: cdn.LocalCDN,
+		remote_cdn: cdn.RemoteCDN,
 		verify: bool=False
 	) -> None:
 		self.version = version
+		self.local_cdn = local_cdn
+		self.remote_cdn = remote_cdn
 		self.verify = verify
 
 		self.product_config_queue = FetchQueue(ProductConfigFetchDirective)
@@ -155,37 +132,31 @@ class Fetcher:
 
 		self.decryption_key: Optional[ArmadilloKey] = None
 
-	def fetch_config(
-		self,
-		local_cdn: cdn.LocalCDN,
-		remote_cdn: cdn.RemoteCDN
-	) -> Generator[Drain, None, None]:
+	def fetch_config(self) -> Generator[Drain, None, None]:
 		product_config_key = self.version.product_config
 		self.product_config_queue.add(product_config_key)
-		yield Drain("product config", self.product_config_queue, local_cdn, remote_cdn)
-		if self.version.product_config and local_cdn.has_config_item(product_config_key):
-			self.product_config = local_cdn.get_product_config(product_config_key)
+		yield Drain("product config", self.product_config_queue, self)
+		if self.version.product_config and self.local_cdn.has_config_item(product_config_key):
+			self.product_config = self.local_cdn.get_product_config(product_config_key)
 
 		if self.product_config:
 			decryption_key_name = self.product_config.get("all", {}).get("config", {}).get(
 				"decryption_key_name", ""
 			)
 			if decryption_key_name:
-				self.decryption_key = local_cdn.get_decryption_key(decryption_key_name)
+				self.decryption_key = self.local_cdn.get_decryption_key(decryption_key_name)
 
 		self.config_queue.add(self.version.build_config)
 		self.config_queue.add(self.version.cdn_config)
-		yield Drain(
-			"config items", self.config_queue, local_cdn, remote_cdn, self.decryption_key
-		)
+		yield Drain("config items", self.config_queue, self)
 
-		if local_cdn.has_config(self.version.build_config):
-			self.build_config = local_cdn.get_build_config(
+		if self.local_cdn.has_config(self.version.build_config):
+			self.build_config = self.local_cdn.get_build_config(
 				self.version.build_config, verify=self.verify
 			)
 
-		if local_cdn.has_config(self.version.cdn_config):
-			self.cdn_config = local_cdn.get_cdn_config(
+		if self.local_cdn.has_config(self.version.cdn_config):
+			self.cdn_config = self.local_cdn.get_cdn_config(
 				self.version.cdn_config, verify=self.verify
 			)
 
@@ -193,18 +164,14 @@ class Fetcher:
 			patch_config_key = self.build_config.patch_config
 			if patch_config_key:
 				self.config_queue.add(patch_config_key)
-				yield Drain("patch config", self.config_queue, local_cdn, remote_cdn)
-				if local_cdn.has_config(patch_config_key):
-					self.patch_config = local_cdn.get_patch_config(
+				yield Drain("patch config", self.config_queue, self)
+				if self.local_cdn.has_config(patch_config_key):
+					self.patch_config = self.local_cdn.get_patch_config(
 						patch_config_key, verify=self.verify
 					)
 
-	def fetch_metadata(
-		self,
-		local_cdn: cdn.LocalCDN,
-		remote_cdn: cdn.RemoteCDN
-	) -> Generator[Drain, None, None]:
-		yield from self.fetch_config(local_cdn, remote_cdn)
+	def fetch_metadata(self) -> Generator[Drain, None, None]:
+		yield from self.fetch_config()
 
 		if self.cdn_config:
 			for archive_key in self.cdn_config.archives:
@@ -214,7 +181,7 @@ class Fetcher:
 			if self.cdn_config.file_index:
 				self.index_queue.add(self.cdn_config.file_index)
 
-			yield Drain("archive indices", self.index_queue, local_cdn, remote_cdn)
+			yield Drain("archive indices", self.index_queue, self)
 
 			for patch_archive_key in self.cdn_config.patch_archives:
 				self.patch_queue.add(patch_archive_key)
@@ -232,10 +199,10 @@ class Fetcher:
 			encoding = self.build_config.encoding
 			if encoding.encoding_key:
 				self.loose_file_queue.add(encoding.encoding_key)
-				yield Drain("encoding file", self.loose_file_queue, local_cdn, remote_cdn)
-				if local_cdn.has_data(encoding.encoding_key):
+				yield Drain("encoding file", self.loose_file_queue, self)
+				if self.local_cdn.has_data(encoding.encoding_key):
 					data = blte.load(
-						local_cdn.download_data(encoding.encoding_key),
+						self.local_cdn.download_data(encoding.encoding_key),
 						encoding.encoding_key,
 						verify=self.verify
 					)
@@ -245,21 +212,17 @@ class Fetcher:
 
 			if self.build_config.size.encoding_key:
 				self.loose_file_queue.add(self.build_config.size.encoding_key)
-				yield Drain("size file", self.loose_file_queue, local_cdn, remote_cdn)
+				yield Drain("size file", self.loose_file_queue, self)
 
 		if self.patch_index_queue:
-			yield Drain("patch indices", self.patch_index_queue, local_cdn, remote_cdn)
+			yield Drain("patch indices", self.patch_index_queue, self)
 
-	def fetch_data(
-		self,
-		local_cdn: cdn.LocalCDN,
-		remote_cdn: cdn.RemoteCDN
-	) -> Generator[Drain, None, None]:
+	def fetch_data(self) -> Generator[Drain, None, None]:
 		if self.cdn_config:
 			archive_group = ArchiveGroup(
 				self.cdn_config.archives,
 				self.cdn_config.archive_group,
-				local_cdn,
+				self.local_cdn,
 				verify=self.verify
 			)
 			if self.encoding_file:
@@ -267,6 +230,6 @@ class Fetcher:
 					if not archive_group.has_file(key):
 						self.loose_file_queue.add(key)
 
-		yield Drain("archives", self.archive_queue, local_cdn, remote_cdn)
-		yield Drain("loose files", self.loose_file_queue, local_cdn, remote_cdn)
-		yield Drain("patch files", self.patch_queue, local_cdn, remote_cdn)
+		yield Drain("archives", self.archive_queue, self)
+		yield Drain("loose files", self.loose_file_queue, self)
+		yield Drain("patch files", self.patch_queue, self)
