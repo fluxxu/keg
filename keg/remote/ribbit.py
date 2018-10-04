e59075ed5442126bfbd62ee98cea2c9f78e01fcb
Jerome Leclanche <jerome@leclan.ch>
2018-10-04 10:42:21 +0300

    Separate concept of the ribbit remote and the ribbit server

--- keg/remote/ribbit.py
+++ keg/remote/ribbit.py
@@ -9,17 +9,19 @@ class RibbitRemote(BaseRemote):
 	supports_blobs = False
 
 	def __init__(self, remote: str, verify: bool = False) -> None:
+		super().__init__(remote)
+
 		url = urlparse(remote)
 		if url.scheme != "ribbit":
 			raise ValueError(f"Invalid ribbit url: {url!r} (must start with ribbit://)")
 
-		# Initialize with a cleaned remote, and removing the path
-		super().__init__(url._replace(path="").geturl())
+		# Store a cleaned remote, removing the path
+		self.base_remote = url._replace(path="").geturl()
 
 		self.product = url.path.lstrip("/")
 
 	def get_response(self, path: str) -> ribbit.RibbitResponse:
-		return ribbit.get(self.remote + "/" + path)
+		return ribbit.get(self.base_remote + "/" + path)
 
 	def get_blob(self, name: str):
 		raise NotImplementedError(f"Blob {name!r} is not available on Ribbit")
